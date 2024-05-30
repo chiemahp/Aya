e281d4702e63f08f6ef792d2e5101eb715bba7c4
chiemahp <chiemahpauline@gmail.com>
2024-05-30 12:27:30 +0300

    checking the if statement

--- contracts/ExampleContract.sol
+++ contracts/ExampleContract.sol
@@ -2,28 +2,51 @@
 pragma solidity ^0.8.20;
 
 /*contract ERC20Token {
+    address public banker ;//= 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
     mapping(address => uint256) public balances;
+    function setBanker(address _addr)public{
+        banker=_addr;
+    }
 
     function setSomeonesBalance(address owner, uint256 amount) public {
-       // balances[owner] = amount;
+        if (owner == banker) {
+            balances[owner] = amount;
+        }
+        revert("not account owner");
     }
 
-   function transferTokensBetweenAddresses(address sender, address receiver, uint256 amount) public {
-        balances[sender] -= amount;   // deduct/debit the sender's balance
-        balances[receiver] += amount; // credit the reciever's balance
-    }
-    function updateAmount(address any,uint256 amount)public{
-        balances[any]=amount;
+    function transferTokensBetweenAddresses(address sender, address receiver, uint256 amount) public {
+        if (sender == banker) {
+            balances[sender] -= amount;   // deduct/debit the sender's balance
+            balances[receiver] += amount; // credit the reciever's balance
+        }
+        revert("not account owner");
     }
 }*/
 
-contract ExampleContract {
-    function whoami() public view returns (address,address) {
-		address sender = msg.sender;
-        address _Contract =address(this);
-		return (sender, _Contract);
-	}
+
+contract ERC20 {
+    address public banker = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    
+    mapping(address => uint256) public balances;
+
+    function setSomeonesBalance(address owner, uint256 amount) public {
+       // if (msg.sender == banker) {
+            require(msg.sender==banker("not banker"));
+            balances[owner] = amount;
+        }
+        
+    
+        // do nothing
+    }
+
+    function transfer(address receiver, uint256 amount) public {
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
 }
+
 
 
 
